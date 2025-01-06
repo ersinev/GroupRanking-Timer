5c4f96de80b4c73113927bfd56e85a5a1820cb1a
ersinev <ersinevci@hotmail.com>
2025-01-06 15:04:11 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';  // React 18 ve sonrasında creat
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Admin from './Admin';
+import PageNotFound from './PageNotFound';
 
 const root = createRoot(document.getElementById('root'));
 
@@ -11,6 +12,7 @@ root.render(
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/admin" element={<Admin />} />
+      <Route path='*' element = {<PageNotFound/>}/>
     </Routes>
   </Router>
 );
